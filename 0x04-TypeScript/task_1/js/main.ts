547683f8a9f95cc8eff1b11b1dde78cd144395fa
Bitrus Dauda <bitrusdauda9118@gail.com>
2025-11-15 13:52:30 -0800

    commit all tasks

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,24 @@
+// Teacher interface
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
+
+// Example usage
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+};
+
+//console.log(teacher3);
+
 // Directors interface extending Teacher
 interface Directors extends Teacher {
     numberOfReports: number;
